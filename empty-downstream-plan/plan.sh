2b3bfa56201224b26e62cef12c357361f88f69ff
Scott Macfarlane <macfarlane.scott@gmail.com>
2018-11-20 10:31:42 -0800

    Initial commit
    Signed-off-by: Scott Macfarlane <macfarlane.scott@gmail.com>

---
+++ empty-downstream-plan/plan.sh
@@ -0,0 +1,12 @@
+pkg_name="empty-downstream-plan"
+pkg_version="0.0.1"
+pkg_origin="smacfarlane"
+pkg_license=("Apache-2.0")
+pkg_maintainer="Scott Macfarlane"
+pkg_description="Empty Downstream Plan"
+
+pkg_deps=(smacfarlane/empty-plan)
+
+do_build() { :; }
+do_install() { :; }
+
