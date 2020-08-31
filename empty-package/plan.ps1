965136e8ad1e5a4abd1d3b45b5f26cee4889850b
Scott Macfarlane <smacfarlane@chef.io>
2020-08-31 12:47:26 -0700

    Add empty package
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

---
+++ empty-package/plan.ps1
@@ -0,0 +1,13 @@
+$pkg_name="empty-plan"
+$pkg_version="0.0.4"
+$pkg_origin="smacfarlane"
+$pkg_license=("Apache-2.0")
+$pkg_maintainer="Scott Macfarlane"
+$pkg_description="Empty Plan"
+
+function Invoke-Before {
+  Write-Host "FOO $foo"
+  Write-Host "env:FOO $env:foo"
+  
+}
+
---
+++ empty-package/plan.sh
@@ -0,0 +1,10 @@
+pkg_name="empty-package"
+pkg_version="0.0.4"
+pkg_origin="smacfarlane"
+pkg_license=("Apache-2.0")
+pkg_maintainer="Scott Macfarlane"
+pkg_description="Empty Plan"
+
+do_build() { :; }
+do_install() { :; }
+
